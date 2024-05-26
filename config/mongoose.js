96815d617c17bf1ac40512f263e5d7da51163692
Imran Khan <imrankhan772244@gmail.com>
2024-05-26 13:34:58 +0530

    uri

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-// mongoose.set('strictQuery',true);
+const dotenv = require('dotenv');
+
+mongoose.set('strictQuery',true);
 // const dotenv = require('.env');
 // const DB = 'mongodb+srv://pawarsubham438:6aO4JOECgoI7Xu71@cluster0.bemtdzm.mongodb.net/mernstack?retryWrites=true&w=majority';
 // dotenv.config({ path: 'config/.env' });
@@ -7,7 +9,7 @@ const mongoose = require('mongoose');
 //connecting mongoose with database
 //I stored MONGODB_URI in my system veriable for security reason. veriable name MONGODB_URI followed by your mongo atlas link
 //for local use you can write this code
-mongoose.connect('mongodb+srv://imrankhan:imran@000@cluster0.avozraf.mongodb.net/placement-cell?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://127.0.0.1:27017/placement-cell');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/placement-cell');
 
 // mongoose.connect(DB, {
 // 	useNewUrlParser: true,
